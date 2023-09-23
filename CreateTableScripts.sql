ab706227f0d878341e715875b9c87224643fd2de
Wes Mitchell <wmdrums@gmail.com>
2023-09-23 10:43:54 -0500

    in memory unit tests for user repo

--- CreateTableScripts.sql
+++ CreateTableScripts.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Users (
     Id int IDENTITY(1,1) NOT NULL,
-    UserName varchar(255) NOT NULL,
-    Email varchar(255) NOT NULL,
+    UserName varchar(255) UNIQUE NOT NULL,
+    Email varchar(255) UNIQUE NOT NULL,
     IsAdmin BIT NOT NULL,
     UserPassword varchar(255) NOT NULL,
     CONSTRAINT PK_User PRIMARY KEY (Id)
-);
+)
 
 CREATE TABLE Videos (
 	Id int IDENTITY(1,1) NOT NULL,
--- MillerTime.Tests/Helpers.cs
+++ MillerTime.Tests/Helpers.cs
@@ -1,16 +1,39 @@
-﻿using MillerTime.API.Models.DBModels;
+﻿using Microsoft.Extensions.Configuration;
+using MillerTime.API.Models.DBModels;
 
 namespace MillerTime.Tests
 {
     public class Helpers
     {
-        public static Video CreateVideo(int userId = 1, string embedUrl = "https://m.youtube.com/watch?v=lalOy8Mbfdc") {
+        public static Video CreateVideo(int userId = 1, string embedUrl = "https://m.youtube.com/watch?v=lalOy8Mbfdc")
+        {
             return new Video
             {
                 Id = 0,
                 UserId = userId,
                 EmbedUrl = embedUrl
             };
+        }
+
+        public static User CreateUser(int id = 0, string username = "TestUserName", string email = "donatello@tmnt.com", bool isAdmin = false, string userPass = "test-pass123#")
+        {
+            return new User
+            {
+
+                Id = id,
+                UserName = username,
+                Email = email,
+                IsAdmin = isAdmin,
+                UserPassword = userPass
+            };
+        }
+
+        public static ConfigurationManager GetConfigFile()
+        {
+            var config = new ConfigurationManager();
+            config.AddJsonFile("C:\\Users\\wmdru\\workspace\\MillerTime.API\\MillerTime.Tests\\testSettings.json",
+                false, true).Build();
+            return config;
         }
     }
 }
