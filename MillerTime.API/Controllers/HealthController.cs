a27c5e7c6e79b93b4d08d409d36c5ee607165bf0
Wes Mitchell <wmdrums@gmail.com>
2025-01-30 15:59:13 -0600

    health check for function to call

---
+++ MillerTime.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MillerTime.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CheckHealth()
+        {
+            return Ok("API is running");
+        }
+    }
+}
