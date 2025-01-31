48d86fee3b820082f55867e4d1d662bbb6d7489a
Wes Mitchell <wmdrums@gmail.com>
2025-01-31 14:56:15 -0600

    increase timeout

--- MillerTime.API/Program.cs
+++ MillerTime.API/Program.cs
@@ -27,7 +27,10 @@ builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
 builder.Services.AddDbContext<MillerTimeContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration["AZURE_SQL_CONNECTIONSTRING"]);
+    options.UseSqlServer(builder.Configuration["AZURE_SQL_CONNECTIONSTRING"], sqlOptions =>
+    {
+        sqlOptions.CommandTimeout(90);
+    });
 });
 builder.Services.AddCors(options =>
 {
